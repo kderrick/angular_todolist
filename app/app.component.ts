6356aa743ffcebfced5ad6f7be86b67b5513c4b4
Joey Arnstein and Kyle Derrick <kderrick1988@gmail.com>
2016-03-22 15:44:20 -0700

    complete to do list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,21 @@
 import { Component } from 'angular2/core';
+import {Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 
-@Compnent({
-  selector: 'task-list',
-  inputs: [taskList],
-  template: `
-  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
-    {{ currentTask.description }}
-  </h3>
-  `
-})
 
-export class TaskListComponent {
-  public taskList: Task[];
-  taskClicked(clickedTask: Task): void{
-    console.log(clickedTask);
-  }
-}
+
+
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
-      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)"> {{ task.description }} </h3>
+      <h1>To-Do List</h1>
+      <task-list [taskList]="tasks"
+      (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
     <div>
   `
 })
@@ -37,13 +30,7 @@ export class AppComponent {
     ];
   }
   taskWasSelected(clickedTask: Task): void{
-    console.log(clickedTask);
-  }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number){
+    console.log("parent",clickedTask);
 
   }
 }
